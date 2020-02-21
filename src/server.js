5e311fda746ee62c8f06b483638e1cd2f178c966
hamin <ggamini7@gmail.com>
2020-02-22 01:43:24 +0900

    1.1.Creating GraphQL Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log("hello");
+require("dotenv").config();
+import { GraphQLServer} from "graphql-yoga"  // graphql-yoga에서 GraphQLServer를 불러와야 함.
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query{
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query:{
+        hello: () => "Hi"
+    }
+};
+
+const server = new GraphQLServer({typeDefs, resolvers});
+
+server.start({port: PORT}, () => 
+    console.log(`Server running on http://localhost:${PORT}`)
+);
