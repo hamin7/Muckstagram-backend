d53dac240c2c9b8c471531da3a9ad3c4e983990a
hamin <ggamini7@gmail.com>
2020-02-28 23:59:09 +0900

    2.3. Integrating Prisma in our Server

--- src/api/Greetings/sayHello/sayHello.js
+++ src/api/Greetings/sayHello/sayHello.js
@@ -1,5 +1,10 @@
+import {prisma} from "../../../../generated/prisma-client";
 export default {
     Query:{
-        sayHello: () => "Hello"
+        // sayHello를 실행하면 HELLO가 리턴되도록.
+        sayHello: async() => {
+            console.log(await prisma.users());  //모든 사용자들을 console에 보여줌.
+            return "HELLO";
+        }
     }
-}
+};
