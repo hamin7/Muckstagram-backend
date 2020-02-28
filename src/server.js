c797996ac0e5e0e0d03194a9fe61da610c9957f9
hamin <ggamini7@gmail.com>
2020-02-29 01:27:28 +0900

    edit server.js

--- src/server.js
+++ src/server.js
@@ -10,5 +10,5 @@ const server = new GraphQLServer({ schema });
 server.express.use(logger("dev"));
 
 server.start({port: PORT}, () => 
-    console.log(`👍🏼Server running on http://localhost:${PORT}`)
+    console.log(`🟢 Server running on http://localhost:${PORT}`)
 );
