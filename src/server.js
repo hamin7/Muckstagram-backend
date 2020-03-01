ef3ddcbc493cf32d8d6b37d9205ccde23a72887c
hamin <ggamini7@gmail.com>
2020-03-01 23:09:41 +0900

    3.3. sendMail Function with Nodemailer

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { GraphQLServer} from "graphql-yoga"  // graphql-yoga에서 GraphQLServer를 불러와야 함.
 import logger from "morgan";
 import schema from "./schema";
+
+import { sendSecretMail } from "./utils";
+
+sendSecretMail("ggamini7@gmail.com", "123");
 
 const PORT = process.env.PORT || 4000;
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,36 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+console.log(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD)
+
+// 아래 email은 email 주소만 말하는게 아니라 보내는데에 필요한 모든것을 포함.
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "hamin@mucstagram.com",
+    to: adress,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`
+  };
+  return sendMail(email);
 };
