73a884f71153b4cb48c5b73f7f4d9eb750769dff
hamin <ggamini7@gmail.com>
2020-03-03 02:09:21 +0900

    middle of 3.6

--- src/passport.js
+++ src/passport.js
@@ -1,6 +1,6 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+// import dotenv from "dotenv";
+// import path from "path";
+// dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 import passport from "passport";
 import { Strategy, ExtractJwt } from "passport-jwt";
@@ -27,4 +27,20 @@ const verifyUser = async (payload, done) => {
     }
 };
 
+// 미들웨어 함수, 토큰을 받아서 작업할 것. passport authenticate를 리턴.
+// verifyUser함수 실행 => passport가 authenticatJwt함수에 사용자 정보를 전달 => 사용자 정보를 req 객체에 붙여줌.
+export const authenticateJwt = (req, res, next) =>
+    // passport에 어떤 것도 입력되지 않기를 원하므로 session: false 옵션을 추가.
+  passport.authenticate("jwt", { sessions: false }, (error, user) => {
+    if (user) {
+        // 사용자가 존재 한다면
+        req.user = user;
+    }
+    // 사용자가 없다면
+    next();
+  })(req, res, next);   // 함수를 리턴, 실행하고 나면 다음 함수로 넘어가는것. 리턴된 함수를 (req, res, next)로 실행.
+  // 이 경우에는 그 실행해야 하는 함수가 graphql 함수.
+
 passport.use(new Strategy(jwtOptions, verifyUser));
+
+passport.initialize();
--- src/server.js
+++ src/server.js
@@ -1,21 +1,30 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+// import dotenv from "dotenv";
+// import path from "path";
+// dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+import "./env";
 
 import { GraphQLServer} from "graphql-yoga"  // graphql-yoga에서 GraphQLServer를 불러와야 함.
 import logger from "morgan";
 import passport from "passport";
 import schema from "./schema";
 import "./passport";    // server.js에서는 passport.js 파일에서 무언가를 받아서 사용할 필요 없기 때문.
+import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+// const server = new GraphQLServer({ schema });
+
+const server = new GraphQLServer({
+    schema,
+    context: ({ request }) => ({ request }) // context는 resolver 사이에서 정보를 공유할 때 사용.
+  });
 
 server.express.use(logger("dev"));
-server.express.use(passport.authenticate("jwt"));   // server.express로 미들웨어를 사용.
+// server.express.use(passport.authenticate("jwt"));   // server.express로 미들웨어를 사용.
 // path를 입력하는데 경로를 미들웨어로 보호하고 싶을 때 사용.
 // express middleware에 원하는 것들을 입력하고 난 후에, 주소 미들웨어를 입력.
+server.express.use(authenticateJwt);
 
 server.start({port: PORT}, () => 
     console.log(`🟢 Server running on http://localhost:${PORT}`)
