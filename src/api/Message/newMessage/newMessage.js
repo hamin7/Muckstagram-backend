9377be59ef10a19e25f105b78ffa4f1aa8672e09
hamin <ggamini7@gmail.com>
2020-03-10 15:13:36 +0900

    29. Conclusion

--- src/api/Message/newMessage/newMessage.js
+++ src/api/Message/newMessage/newMessage.js
@@ -6,16 +6,18 @@ export default {
       subscribe: (_, args) => {
         const { roomId } = args;
         // prisma에서 subscribe 할 수 있는 object를 리턴.
-        return prisma.$subscribe.message({
-          AND: [
-            { mutation_in: "CREATED" },
-            {
-              node: {
-                room: { id: roomId }
+        return prisma.$subscribe
+          .message({
+            AND: [
+              { mutation_in: "CREATED" },
+              {
+                node: {
+                  room: { id: roomId }
+                }
               }
-            }
-          ]
-        });
+            ]
+          })
+          .node();
       },
       resolve: payload => payload
     }
