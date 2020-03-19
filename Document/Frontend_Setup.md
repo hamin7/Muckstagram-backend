af5bf8fc51f49fe826c105e4caf9d37279110bb3
hamin <ggamini7@gmail.com>
2020-03-19 21:39:41 +0900

    create account refactoring

---
+++ Document/Frontend_Setup.md
@@ -0,0 +1,6 @@
+## Frontend Setup
+
+<ol>
+    <li>폴더 생성
+    <li>npx create-react-app Muckstagram-frontend
+</ol>
--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -2,17 +2,21 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Mutation: {
-        createAccount: async(_, args) => {
+        createAccount: async (_, args) => {
             // 빈 string을 선언하는 이유는 null 같은 이상한 타입이 들어오지 않도록 막기 위해서.
-            const { username, email, firstName = "", lastName = "", bio = ""} = args;  // password는 입력받지 않을 것이다.
-            const user = await prisma.createUser({
+            const { username, email, firstName = "", lastName = "", bio = "" } = args;  // password는 입력받지 않을 것이다.
+            const exists = await prisma.$exists.user({ username });
+            if (exists) {
+                throw Error("This username is already taken");
+            }
+            await prisma.createUser({
                 username,
                 email,
                 firstName,
                 lastName,
                 bio
             });
-            return user;
+            return true;
         }
     }
 };
