6b3432e0246fad4a78f54a68d97bea27138741e9
hamin <ggamini7@gmail.com>
2020-03-20 01:52:28 +0900

    refactoring createAccount.js

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -5,9 +5,16 @@ export default {
         createAccount: async (_, args) => {
             // 빈 string을 선언하는 이유는 null 같은 이상한 타입이 들어오지 않도록 막기 위해서.
             const { username, email, firstName = "", lastName = "", bio = "" } = args;  // password는 입력받지 않을 것이다.
-            const exists = await prisma.$exists.user({ username });
+            const exists = await prisma.$exists.user({ 
+                OR: [
+                    {
+                        username
+                    },
+                    { email }
+                ]
+            });
             if (exists) {
-                throw Error("This username is already taken");
+                throw Error("This username / email is already taken");
             }
             await prisma.createUser({
                 username,
