18a40552afb0b59685fcd971b9361793ca974e83
hamin <ggamini7@gmail.com>
2020-03-24 22:42:23 +0900

    edit me.js

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -7,12 +7,8 @@ export default {
         // DoubleUnderScore는 부모의 arguments를 뜻함.
       isAuthenticated(request);
       const { user } = request;
-      const userProfile = await prisma.user({ id: user.id });
-      const posts = await prisma.user({ id: user.id }).posts(); //만약 user의 posts만 받아오고 싶다면, user를 받을 필요 없다면 $fragment 대신에 .posts()를 사용.
-      return {
-        user: userProfile,
-        posts
-      };
+      return await prisma.user({ id: user.id });
+      // const posts = await prisma.user({ id: user.id }).posts(); //만약 user의 posts만 받아오고 싶다면, user를 받을 필요 없다면 $fragment 대신에 .posts()를 사용.
     }
   },
 };
